7ef9b7cbcf7ae8d699ab3a360c8dfd04e9c172e7
Будаев Иван <budaevivan2003@gmail.com>
2021-10-17 00:47:12 +0500

    наконец-то закончил код

--- Homework4/Homework4/Program.cs
+++ Homework4/Homework4/Program.cs
@@ -4,26 +4,36 @@ namespace Homework4
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
 
         {
             double x;
-            
 
-            double sin2 = Math.Sin(2);
-            double sin3 = Math.Sin(3);
-            double sin5 = Math.Sin(5);
+            Console.WriteLine(Math.Round(value: x = Conclusion, digits: 3));
 
-            double cos5 = Math.Cos(5);
-            double cos11 = Math.Cos(11);
 
-            Console.WriteLine(Math.Round(x = ((2 + sin2 / 5 + cos5) + (3 + sin3 / 7 + cos5) + (5 + sin5 / 11 + cos11)), 3));
+            Console.ReadKey();
 
-            
 
-            Console.ReadKey();       
+           
         }
 
+        static double Conclusion
+        {
+            get
+            {
+                double x;
+
+                double sin2 = Math.Sin(2);
+                double sin3 = Math.Sin(3);
+                double sin5 = Math.Sin(5);
+
+                double cos5 = Math.Cos(5);
+                double cos11 = Math.Cos(11);
+
+                return x = ((2 + sin2 / 5 + cos5) + (3 + sin3 / 7 + cos5) + (5 + sin5 / 11 + cos11));
+            }
+        }
     }
 
 }
