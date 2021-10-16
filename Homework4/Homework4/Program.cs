80eaf179d10eeca0021369f941649821f62dd420
Будаев Иван <budaevivan2003@gmail.com>
2021-10-16 23:13:33 +0500

    Homework4 was ended

---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main()
+
+        {
+            double x;
+            
+
+            double sin2 = Math.Sin(2);
+            double sin3 = Math.Sin(3);
+            double sin5 = Math.Sin(5);
+
+            double cos5 = Math.Cos(5);
+            double cos11 = Math.Cos(11);
+
+            Console.WriteLine(Math.Round(x = ((2 + sin2 / 5 + cos5) + (3 + sin3 / 7 + cos5) + (5 + sin5 / 11 + cos11)), 3));
+
+            
+
+            Console.ReadKey();       
+        }
+
+    }
+
+}
