75848daef1f995e62e82b7c7505c430bb954a91c
Будаев Иван <budaevivan2003@gmail.com>
2021-10-27 01:10:50 +0500

    закончил задание 5.2

---
+++ homework5.2/homework5.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace homework5._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //    Слово апельсин,  пень и спаниель
+            string str = "";
+            
+            str = "Апельсин"; //а-0 п-1 е-2 л-3 ь-4 с-5 и-6 н-7
+            Console.WriteLine(str);
+           
+            string result = str.Substring(1, 2)
+                + str.Substring(7)
+                + str.Substring(4, 1);
+
+            
+            Console.WriteLine(result);
+            string resalt = str.Substring(5, 1)
+                + str.Substring(1, 1)
+                + str.Substring(0, 1).ToLower()
+                + str.Substring(7)
+                + str.Substring(6, 1)
+                + str.Substring(2, 3);
+
+            Console.WriteLine(resalt);
+            Console.ReadKey();
+
+        }
+    }
+}
