d2a4220783ffe8daf2d2814fa76f38a0a293814c
Будаев Иван <budaevivan2003@gmail.com>
2021-11-07 14:38:56 +0500

    исправил задиние 4

---
+++ Homework4 ed/Homework4 ed/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework4_ed
+{
+    class Program
+    {
+        static double Metod(double q, double w)
+        {
+            return ((q + Math.Sin(q)) / (w + Math.Cos(w)));
+
+        }
+        static void Main(string[] args)
+        {
+            double x = Metod(2, 5) + Metod(3, 5) + Metod(5, 11);
+            Console.WriteLine($"x = {x:F3}");
+            Console.ReadKey();
+
+        }
+    }
+}
