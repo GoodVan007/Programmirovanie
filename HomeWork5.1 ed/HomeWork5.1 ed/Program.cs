7b399f680642111826501b0b9c39d0c0cb243d6e
Будаев Иван <budaevivan2003@gmail.com>
2021-11-07 23:12:33 +0500

    Еще раз Исправил Задание 5.1

--- HomeWork5.1 ed/HomeWork5.1 ed/Program.cs
+++ HomeWork5.1 ed/HomeWork5.1 ed/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace HomeWork5._1_ed
 {
@@ -11,40 +8,47 @@ namespace HomeWork5._1_ed
         static void Main(string[] args)
         {
             Console.WriteLine("write Eng. text");
-            
-            string text = Console.ReadLine().ToLower();
-            
-            text = text.Replace("a", "4");
-            text = text.Replace("e", "3");
-            text = text.Replace("i", "!");
-            text = text.Replace("q", "9");
-            text = text.Replace("b", "6");
-            text = text.Replace("f", "|=");
-            text = text.Replace("j", ")");
-            text = text.Replace("r", "|2");
-            text = text.Replace("z", "2");
-            text = text.Replace("c", "(");
-            text = text.Replace("g", "9");
-            text = text.Replace("k", "|<");
-            text = text.Replace("o", "0");
-            text = text.Replace("s", "5");
-            text = text.Replace("d", "|)");
-            text = text.Replace("h", "|-|");
-            text = text.Replace("p", "|>");
-            text = text.Replace("l", "1");
-            text = text.Replace("t", "7");
-            text = text.Replace("x", "><");
-            text = text.Replace("u", "|_|");
-            text = text.Replace("v", @"\/"); 
-            text = text.Replace("m", @"|\/|");
-            text = text.Replace("w", @"\/\/");
-            text = text.Replace("n", @"|\|");
-            text = text.Replace("y", "'/");
 
-            Console.WriteLine(text);
+
+            string text = Console.ReadLine().ToLower();
+
+            Console.WriteLine(Leet(text));
 
             Console.ReadKey();
         }
 
+        static string Leet(string x)
+        {
+            
+
+            x = x.Replace("a", "4");
+            x = x.Replace("e", "3");
+            x = x.Replace("i", "!");
+            x = x.Replace("q", "9");
+            x = x.Replace("b", "6");
+            x = x.Replace("f", "|=");
+            x = x.Replace("j", ")");
+            x = x.Replace("r", "|2");
+            x = x.Replace("z", "2");
+            x = x.Replace("c", "(");
+            x = x.Replace("g", "9");
+            x = x.Replace("k", "|<");
+            x = x.Replace("o", "0");
+            x = x.Replace("s", "5");
+            x = x.Replace("d", "|)");
+            x = x.Replace("h", "|-|");
+            x = x.Replace("p", "|>");
+            x = x.Replace("l", "1");
+            x = x.Replace("t", "7");
+            x = x.Replace("x", "><");
+            x = x.Replace("u", "|_|");
+            x = x.Replace("v", @"\/");
+            x = x.Replace("m", @"|\/|");
+            x = x.Replace("w", @"\/\/");
+            x = x.Replace("n", @"|\|");
+            x = x.Replace("y", "'/");
+
+            return x;
+        }
     }
 }
