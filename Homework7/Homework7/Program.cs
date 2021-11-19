7029f72596189cb0317f1f8556328491bf7f5607
Будаев Иван <budaevivan2003@gmail.com>
2021-11-19 14:22:26 +0500

    Закончил задание 7

---
+++ Homework7/Homework7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение X : ");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Значение функции f(x) = {Metod(x)}");
+            Console.ReadKey();
+        }
+        static double Metod(double x)
+        {
+            double f;
+            if ((Math.Abs(Math.Sin(x)) < x))
+                f = -Math.Sin(x);
+            else
+            {
+                if ((Math.Abs(Math.Sin(x)) == x))
+                    f = 0;
+                else
+                    f = Math.Sin(x);
+            }
+            return f;
+        }
+    }
+}
