5e2a53b8707f28ee23392ac0fcd3db098854f5b3
Будаев Иван <budaevivan2003@gmail.com>
2021-12-24 16:25:50 +0500

    Homework8 задание 1 и 4

---
+++ homework8/homework8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace homework8
+{
+    class Program
+    {//Задание 4
+        static void MainFour()
+        {
+            var count = 0;
+            Console.WriteLine("Введите натуральное число n");
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите натуральное число k(0 <= k <= 8");
+            var k = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                var a = n % 10;
+                if (a > k)
+                    count += a;
+                n /= 10;
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+        //задание 1
+        static void MainOne()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0.0;
+            for (double i = 1; i <= n; i++)
+            {
+                sum += i / (i + 1);
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+
+}
